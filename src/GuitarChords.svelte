998c28ccaaeda158a12ebc4588dc83f9d95ffddb
ivosdc <ibozic@mail.de>
2022-04-10 00:21:44 +0200

    scroll-row width

--- src/GuitarChords.svelte
+++ src/GuitarChords.svelte
@@ -179,7 +179,7 @@
         flex-wrap: nowrap;
         overflow-x: auto;
         border: none;
-        width: 100vw;
+        width: calc(100vw - 6rem);
         gap: 0.2rem;
         padding:0 3rem 0.5rem;
     }
